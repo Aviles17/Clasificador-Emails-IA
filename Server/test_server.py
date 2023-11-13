b088eade2326d60f82ceec050e930f75da98e979
Aviles17 <santiaviles17@gmail.com>
2023-11-13 13:21:05 -0500

    Reduccion de Falsos Negativos a 0, nuevo modelo de IA con repositorio de Kaggle, modificación de archivos binarios y nuevas metricas de modelo

--- Server/test_server.py
+++ Server/test_server.py
@@ -7,7 +7,6 @@ class TestServer(unittest.TestCase):
     #Inicializar el servidor para las pruebas unitarias
     def setUp(self):
         self.app = app.test_client()
-    
     #Prueba de que el servidor fue ejecutado correctamente y el root esta desplegado  
     def test_server_is_up(self):
         response = self.app.get('/')
@@ -18,7 +17,9 @@ class TestServer(unittest.TestCase):
         with open('Server/recursos/email_1.json') as f:
            test_data = json.load(f)
         response = self.app.post('/NaiveBayesPredict', json=test_data)
-        self.assertEqual(response.status_code, 200)
+        c_good_response = response.status_code == 200
+        c_good_predict = response.text == "0.0"
+        self.assertEqual(c_good_response and c_good_predict, True)
 
 if __name__ == '__main__':
   unittest.main()
